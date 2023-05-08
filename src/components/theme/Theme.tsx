c6de98617dc16e3e34693c09b435b6ef3d75a6a6
AlejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2023-05-07 21:39:00 -0300

    modal theming

--- src/components/theme/Theme.tsx
+++ src/components/theme/Theme.tsx
@@ -23,6 +23,10 @@ const theme = createTheme({
       main: '#E03C39',
       light: '#E57472',
     },
+    info: {
+      main: '#D9D9D9',
+      light: '#EDEDED',
+    },
   },
   typography: {
     fontFamily: 'Gotham, sans-serif',
@@ -94,6 +98,32 @@ const theme = createTheme({
         children: (props: any) => <Toolbar>{props.children}</Toolbar>,
       },
       styleOverrides: {},
+    },
+    MuiDialog: {
+      styleOverrides: {
+        paper: {
+          borderRadius: '8px',
+          borderTop: '4px solid' + COLORS.primary.main,
+          boxShadow: 'none',
+          maxHeight: 'none',
+          maxWidth: 'none',
+        },
+      },
+    },
+    MuiDialogTitle: {
+      styleOverrides: {
+        root: {
+          fontWeight: 'bold',
+          fontSize: '16px',
+        },
+      },
+    },
+    MuiDialogActions: {
+      styleOverrides: {
+        root: {
+          background: COLORS.grey.light,
+        },
+      },
     },
   },
 });
